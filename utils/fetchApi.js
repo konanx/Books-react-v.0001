01c04c6589574ba7acd41c94f4590f88b92956e2
konanx <micha112@onet.pl>
2022-03-15 19:53:28 +0100

    init

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,8 +5,8 @@ export const baseUrl = "https://bookshelves.p.rapidapi.com/books";
 export const fetchApi = async (url, { setBooks }) => {
   const { data } = await axios.get(url, {
     headers: {
-      "x-rapidapi-host": ".d",
-      "x-rapidapi-key": ".d",
+      "x-rapidapi-host": "",
+      "x-rapidapi-key": "",
     },
   });
   setBooks(data.Books);
